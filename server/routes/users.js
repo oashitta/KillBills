0e2bcb339eb9a37df52874e62c47d463e0a9f211
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-06-08 16:41:09 -0400

    initial commit

---
+++ server/routes/users.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+const router = require("express").Router();
+
+const users = ["Bob", "Alex", "Will", "Tristan"];
+
+module.exports = (db) => {
+  // all routes will go here
+  router.get("/", (req, res) => {
+    res.json(users);
+  });
+
+  return router;
+};
