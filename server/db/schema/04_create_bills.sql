bd2c08c6a9d132abb346068a7f3fcc65343ee0b5
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-06-08 20:02:41 -0400

    setup database

---
+++ server/db/schema/04_create_bills.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS bills CASCADE;
+CREATE TABLE bills (
+  id SERIAL PRIMARY KEY,
+  payee_id INTEGER REFERENCES payees(id) NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  amount MONEY,
+  due_date DATE,
+  reminder_date DATE,
+  paid_date DATE,
+  status BOOLEAN
+);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,10 @@
-var express = require("express");
-
-const router = require("express").Router();
-
+const express = require("express");
+const router = express.Router();
 const users = ["Bob", "Alex", "Will", "Tristan"];
 
-module.exports = (db) => {
-  // all routes will go here
-  router.get("/", (req, res) => {
-    res.json(users);
-  });
+// all routes will go here
+router.get("/", (req, res) => {
+  res.json(users);
+});
 
-  return router;
-};
+module.exports = router;
