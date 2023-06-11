55ceddbee3a213413a4f8966aeb48b720d9449da
Chansoo Han <devhan1207@gmail.com>
2023-06-11 19:16:48 -0400

    added axios get

--- client/src/components/AddBill.jsx
+++ client/src/components/AddBill.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useFormik } from 'formik';
 
 const AddBill = () => {
+  const [payees, setPayees] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/payees').then(res => {
+      console.log(res.data)
+      setPayees(res.data)
+    })
+  }, [])
+
   const formik = useFormik({
     initialValues: {
       amount: 0,
@@ -13,7 +22,7 @@ const AddBill = () => {
     },
     onSubmit: values => {
       console.log(values)
-      axios.post('http://localhost:8080/users/new', 
+      axios.post('http://localhost:8080/bills', 
       values, {
         headers: {'Content-Type': 'application/json'}
       })
@@ -23,77 +32,30 @@ const AddBill = () => {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600 lg:max-w-xl">
-          <div className="text-xl font-bold text-gray-800">
-            <span>+ add payees</span>
-            <span>- delete payees</span>
-          </div>
-          <form className="mt-6" onSubmit={formik.handleSubmit}>
-            <div className="mb-2">
-              <label
-                htmlFor="payee"
-                className="text-xl font-bold text-gray-800"
-              >
-                Payee: 
-              </label>
-              <select name="dog-names" id="dog-names"> 
-                <option value="rigatoni">Rigatoni</option> 
-                <option value="dave">Dave</option> 
-                <option value="pumpernickel">Pumpernickel</option> 
-                <option value="reeses">Reeses</option> 
-              </select>
-            </div>
-            <div className="mb-2 block text-xl font-semibold text-gray-800">
-              amount
-            </div>
-            <div className='border-2'>
-              due date<br />
-              calendar
-            {/* <label
-              htmlFor="name"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Name
-            </label>
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.name}
-              className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
+        <div className="text-xl font-bold text-gray-800">
+          <span>+ add payees</span>
+          <span>- delete payees</span>
+        </div>
+        <form className="mt-6" onSubmit={formik.handleSubmit}>
           <div className="mb-2">
             <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-gray-800"
+              htmlFor="payee"
+              className="text-xl font-bold text-gray-800"
             >
-              Email
+              Payee: 
             </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
+            <select name="names" id="names"> 
+              {payees.map((payee) => {
+                return <option key={payee.id} value={payee.name}>{payee.name}</option>
+              })}
+            </select>
           </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-              className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            /> */}
+          <div className="mb-2 block text-xl font-semibold text-gray-800">
+            amount
+          </div>
+          <div className='border-2'>
+            due date<br />
+            calendar
           </div>
           <div className="mt-6">
             <button 
