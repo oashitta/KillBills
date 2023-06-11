7d48fabf5a32171db361bab101273e1805676b1f
Chansoo Han <devhan1207@gmail.com>
2023-06-10 23:14:28 -0400

    added post '/new/'

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const users = ["Bob", "Alex", "Will", "Tristan"];
+const usersQuery = require('../db/queries/users')
 
 // all routes will go here
 router.get("/", (req, res) => {
   res.json(users);
 });
+
+router.post('/new', (req, res) => {
+  const {name, email, password} = req.body;
+
+  usersQuery.createUser(name, email, password)
+})
 
 module.exports = router;
