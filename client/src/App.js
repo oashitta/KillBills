827d18a3706f75c75e1f26a8b65766d1091737d8
Chansoo Han <devhan1207@gmail.com>
2023-06-11 22:10:06 -0400

    added AddPayee component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import TopNavigationBar from "./components/TopNavigationBar";
 import Profile from "./components/Profile";
 import "./App.css";
+import AddPayee from './components/AddPayee';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
     >
       <TopNavigationBar />
       <Profile />
+      <AddPayee />
     </Auth0Provider>
   );
 }
