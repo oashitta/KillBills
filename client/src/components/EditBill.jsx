50beeb177d9c7cc2c5600f903b05a7e374634f24
Chansoo Han <devhan1207@gmail.com>
2023-06-12 19:02:46 -0400

    input filed fixed

--- client/src/components/EditBill.jsx
+++ client/src/components/EditBill.jsx
@@ -5,8 +5,16 @@ import { useFormik } from 'formik';
 const EditBill = () => {
   const [payees, setPayees] = useState([]);
   const [bill, setBill] = useState({});
+  const [btnClicked, setBtnClicked] = useState('')
+  const [isPaid, setIsPaid] = useState(false);
 
-  
+  const isPaidHandle = (e) => {
+    e.preventDefault();
+    if(bill.paid_date) {
+      setIsPaid("ture")
+    }
+    setIsPaid(!isPaid)
+  }
 
   useEffect(() => {
     axios.get('http://localhost:8080/payees').then(res => {
@@ -16,7 +24,7 @@ const EditBill = () => {
   }, [])
 
   useEffect(() => {
-    axios.get('http://localhost:8080/bills/5')
+    axios.get('http://localhost:8080/bills/10')
     .then(res => {
       console.log("data", res.data)
 
@@ -48,11 +56,14 @@ const EditBill = () => {
       note: '',
     },
     onSubmit: values => {
-      console.log(values)
-      axios.put('http://localhost:8080/bills/5', 
-      values, {
-        headers: {'Content-Type': 'application/json'}
-      })
+      if(btnClicked === 'edit') {
+        axios.put('http://localhost:8080/bills/5', 
+        values, {
+          headers: {'Content-Type': 'application/json'}
+        })
+      } else {
+        axios.delete('http://localhost:8080/bills/5')
+      }
     }
   });
 
@@ -159,16 +170,41 @@ const EditBill = () => {
               className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
+          <div className='mt-4'>
+            <div className="flex">
+                <label className="inline-flex relative items-center mr-5 cursor-pointer">
+                    <input
+                        type="checkbox"
+                        className="sr-only peer"
+                        checked={isPaid}
+                        readOnly
+                    />
+                    <div
+                        onClick={isPaidHandle}
+                        className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+                    ></div>
+                    <span className="ml-2 text-lg font-medium text-gray-900">
+                        Paid
+                    </span>
+                </label>
+            </div>
+        </div>
           <div className="mt-6">
             <button 
               type="submit"
+              onClick={(e) => {
+                setBtnClicked('edit')
+              }}
               className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
             >
               Edit Bill
             </button>
             <button 
               type="submit"
-              className="w-full mt-2 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+              onClick={(e) => {
+                setBtnClicked('delete')
+              }}
+              className="w-full mt-2 px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-md hover:bg-red-600 focus:outline-none focus:bg-indigo-600"
             >
               Delete Bill
             </button>
