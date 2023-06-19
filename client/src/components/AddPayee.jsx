47ef96429f70eb6dfa233210b65a5b08a6a43689
David Luc <luc.ky@me.com>
2023-06-19 02:59:31 -0400

    Update form layouts

--- client/src/components/AddPayee.jsx
+++ client/src/components/AddPayee.jsx
@@ -65,10 +65,10 @@ const AddPayee = () => {
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md border-solid border-2 border-violet-400 lg:max-w-xl">
+      <div className="w-full p-6 m-auto bg-white rounded-md border-solid border-2 border-indigo-400 lg:max-w-xl mt-12">
         <form className="mt-6" onSubmit={formik.handleSubmit}>
           <div className="mb-2">
-            <label htmlFor="payee" className="text-xl font-bold text-gray-800">
+            <label htmlFor="payee" className="text-md font-bold text-gray-800">
               Category:
             </label>
             <select
@@ -76,14 +76,14 @@ const AddPayee = () => {
               id="categoryId"
               onChange={formik.handleChange}
               value={formik.values.value}
+              className="flex-grow w-full px-4 py-2 mr-2 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
             >
-              {categories.map((category) => {
-                return (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                );
-              })}
+              <option selected>Choose a category</option>
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
             </select>
           </div>
           <input
@@ -108,7 +108,7 @@ const AddPayee = () => {
             id="url"
             name="url"
             type="text"
-            placeholder="Link"
+            placeholder="URL Link"
             onChange={formik.handleChange}
             value={formik.values.url}
             className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
@@ -116,7 +116,7 @@ const AddPayee = () => {
           <div className="mt-6">
             <button
               type="submit"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+              className="px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
             >
               Add Payee
             </button>
