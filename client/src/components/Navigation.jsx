c24261b047e36d21e501e5c25fdd99ed4e3b8b32
David Luc <luc.ky@me.com>
2023-06-20 01:36:03 -0400

    Update logo

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -29,10 +29,7 @@ export default function Navigation() {
                 <div className="flex items-center">
                   <div className="flex-shrink-0 mt-2">
                     <a href="https://killbills.onrender.com" className="-m-1.5 p-1.5">
-                      <span className="text-2xl font-bold text-black">KILL</span>
-                      <span className="text-2xl text-indigo-600">/</span>
-                      <span className="text-2xl font-bold text-black">BILL</span>
-                      <span className="text-2xl text-indigo-600">$</span>
+                      <img src="logo.png" className="h-7" alt="Logo" />
                     </a>
                   </div>
                 </div>
