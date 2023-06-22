64d28e19cfffea3d0cff24157ddd1c91e338caad
David Luc <luc.ky@me.com>
2023-06-22 01:08:58 -0400

    Add notification test

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -19,6 +19,15 @@ const Dashboard = () => {
   const [billsOverdueDates, setBillsOverdueDates] = useState(null);
   const [isAddBillModalOpen, setIsAddBillModalOpen] = useState(false);
 
+  // useEffect(() => {
+  //   OneSignal.initialize('59583636-8a5d-4790-9e2a-07e3d38c2b04', {
+  //     allowLocalhostAsSecureOrigin: true, // Optional: Required only for local development
+  //   });
+
+  //   return () => {
+  //     OneSignal.removeEventListeners();
+  //   };
+  // }, []);
 
   useEffect(() => {
     OneSignal.init({ appId: '59583636-8a5d-4790-9e2a-07e3d38c2b04' });
@@ -28,55 +37,8 @@ const Dashboard = () => {
     console.log("Tagging");
     OneSignal.sendTag("test", tag).then(() => {
       console.log("Tagged");
-    });   
-  }
-
-  useEffect(() => {
-    OneSignal.push(()=> {
-      OneSignal.init(
-        {
-          appId: "1a9bbed2-9294-4192-a736-01b461cb389a", //STEP 9
-          promptOptions: {
-            slidedown: {
-              enabled: true,
-              actionMessage: "We'd like to show you notifications for the latest news and updates about the following categories.",
-              acceptButtonText: "OMG YEEEEESS!",
-              cancelButtonText: "NAHHH",
-              categories: {
-                  tags: [
-                      {
-                          tag: "react",
-                          label: "ReactJS",
-                      },
-                      {
-                        tag: "angular",
-                        label: "Angular",
-                      },
-                      {
-                        tag: "vue",
-                        label: "VueJS",
-                      },
-                      {
-                        tag: "js",
-                        label: "JavaScript",
-                      }
-                  ]
-              }     
-          } 
-        },
-        welcomeNotification: {
-          "title": "One Signal",
-          "message": "Thanks for subscribing!",
-        } 
-      },
-        //Automatically subscribe to the new_app_version tag
-        OneSignal.sendTag("new_app_version", "new_app_version", tagsSent => {
-          // Callback called when tag has finished sending
-          console.log('new_app_version TAG SENT', tagsSent);
-        })
-      );
     });
-  })
+  }  
   
   useEffect(() => {
     const fetchOverdueTotal = async () => {
